2d392ea4de2f9762ea7c6293b99e57c363cd9b4a
Allan Sechrist <sechristallan@gmail.com>
2026-08-11 19:48:26 +0900

    Set up plane

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,2 +1,14 @@
 extends CharacterBody2D
 class_name Player
+
+@export var gravity := 900.0
+@export var click_strength := 500.0
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("click"):
+		velocity.y = -click_strength
+	
+	move_and_slide()
+	
